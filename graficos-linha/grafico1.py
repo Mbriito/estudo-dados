6d465e6f68025ca4cfa4ee5eb47e5a0e8d5db2b1
maria <marbrito070@gmail.com>
2024-07-25 20:20:07 -0300

    "colocando titulo no grafico"

--- graficos-linha/grafico1.py
+++ graficos-linha/grafico1.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
-x = [1, 2]
-y = [2, 3]
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+# Titulo
+plt.title("Grafico de linha")
+#Eixos
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
 plt.plot(x, y)
 plt.show()
