baf0696b8db365a81cb6893a8f002b5da783a01f
maria <marbrito070@gmail.com>
2024-07-25 23:28:47 -0300

    implementando grafico de compraração

--- graf_barras.py
+++ graf_barras.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 7, 1, 0]
+x1 = [1, 3, 5, 7, 9]
+y1 = [2, 3, 7, 1, 0]
+
+x2 = [2, 4, 6, 8, 10]
+y2 = [5, 1, 3, 7, 4]
+
 
 title = "Grafico de Barras"
 eixox = "EixoX"
@@ -12,5 +16,6 @@ plt.title("Grafico de Barras")
 plt.xlabel("Eixo X")
 plt.ylabel("Eixo Y")
 
-plt.bar(x, y)
+plt.bar(x1, y1)
+plt.bar(x2, y2)
 plt.show()
