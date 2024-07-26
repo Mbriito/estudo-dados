799f0425af958d8f2448b45dc3924e774ffe8de7
maria <marbrito070@gmail.com>
2024-07-25 23:45:33 -0300

    "Adicionando legenda ao grafico"

--- graf_barras.py
+++ graf_barras.py
@@ -6,16 +6,18 @@ x2 = [2, 4, 6, 8, 10]
 y2 = [5, 1, 3, 7, 4]
 
 
-title = "Grafico de Barras"
+title = "Grafico de Barras2"
 eixox = "EixoX"
 eixoy = "EixoY"
 
 
-# Titulo#Eixos
-plt.title("Grafico de Barras")
-plt.xlabel("Eixo X")
-plt.ylabel("Eixo Y")
+# Legenda
+plt.title(title)
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
 
-plt.bar(x1, y1)
-plt.bar(x2, y2)
+plt.bar(x1, y1, label = "Grupo1")
+plt.bar(x2, y2, label = "Grupo2")
+plt.legend()
+
 plt.show()
